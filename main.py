176cc7d913e3e240329d11902b72e69aaa1df746
enzod3 <enzojdefranco@gmail.com>
2023-12-05 01:06:03 -0500

    MVP

--- main.py
+++ main.py
@@ -18,9 +18,21 @@ def onAppStart(app: any):
 
     app.primaryFontSize = 14
     app.secondaryFontSize = 12
-    app.navbar = NavBar(app,300)
-    app.courseview = CourseView(app,300)
-    app.scheduleview = ScheduleView(app,300)
+    app.navbar = NavBar(app,350)
+    app.courseview = CourseView(app,350)
+    app.scheduleview = ScheduleView(app,350)
+    app.popupUnitButton = Button(lambda: app.state.update({"unitPopup":False}) ,app.width//2 - 20,app.height//2 + 20,40,20,fill=rgb(45,45,45))
+    app.popupCourseSaveButton = Button(lambda: (app.state.update({"courseEditPopup":False}),
+                                                app.state["editPopupCourse"].setWorkload(app.state["editPopupCourseWorkloadInput"]),
+                                                app.state["editPopupCourse"].updateSectionWorkloads(app.state["editPopupCourseWorkloadInput"]),
+                                                app.state.update({"editPopupCourseWorkloadInput":""}),
+                                                generateSchedules(app)),
+                                                app.width//2 - 40,app.height//2 + 66,100,24,fill=rgb(45,45,45))
+    app.popupCourseCancelButton = Button(lambda: app.state.update({"courseEditPopup":False}),app.width//2 +65,app.height//2 + 66,50,24,fill=rgb(45,45,45))
+
+    app.workloadWeight = 0.33
+    app.ratingWeight = 0.33
+    app.breakWeight = 0.33
     # should be loaded/stored
     #------------- APP STORAGE --------------
     app.schedules = []
@@ -37,6 +49,13 @@ def onAppStart(app: any):
     app.state["courseInputError"] = ""
     app.state["selectedScheduleIndex"] = 0
     app.state["schedulePage"] = 0
+    app.state["unitPopup"] = False
+    app.state["unitPopupCourseID"] = ""
+    app.state["unitPopupInput"] = ""
+
+    app.state["courseEditPopup"] = False
+    app.state["editPopupCourse"] = None
+    app.state["editPopupCourseWorkloadInput"] = ""
     #-----------------------------------------
     with open('./data/courses.txt', 'r') as file:
         for line in file:
@@ -101,6 +120,7 @@ class Section(): #sections/recitations
         self.units = units
         self.color = color
         self.section = section
+        self.overrideWorkload = None
         self.days = set()
         self.addDays(days,begin,end,bld_room,location,instructors)
     def __repr__(self):
@@ -138,20 +158,45 @@ class Lecture(): #Every Class has a lecture taught by main professor --> some co
     def __init__(self,lecture: Section, sections: Optional[list[Section]] = []):
         self.lecture = lecture
         self.sections = set(sections)
+    def __copy__(self): #https://stackoverflow.com/questions/1500718/how-to-override-the-copy-deepcopy-operations-for-a-python-object
+        cls = self.__class__
+        result = cls.__new__(cls)
+        result.__dict__.update(self.__dict__)
+        return result
+    def __deepcopy__(self, memo): #https://stackoverflow.com/questions/1500718/how-to-override-the-copy-deepcopy-operations-for-a-python-object
+        cls = self.__class__
+        result = cls.__new__(cls)
+        memo[id(self)] = result
+        for k, v in self.__dict__.items():
+            if k == 'app':
+                setattr(result, k, self.app)
+            else:
+                setattr(result, k, deepcopy(v, memo))
+        return result
 
 class Course():
     def __init__(self,courseID: str, title:str,units:float,lectures: list[Lecture],color):
         self.courseID = courseID
         self.title = title
         self.units = units
+        self.workload = None
         self.lectures = lectures
         self.color = color
     def __repr__(self):
         return f"{self.courseID}"
     def __eq__(self,other):
-        return isinstance(other,type(self)) and ((self.courseID,self.title,self.units,self.lectures,self.sections)==(other.courseID,other.title,other.units,other.lectures,other.sections))
+        print(((self.courseID,self.title,self.units,self.lectures),(other.courseID,other.title,other.units,other.lectures)))
+        return isinstance(other,type(self)) and ((self.courseID,self.title,self.units,self.lectures)==(other.courseID,other.title,other.units,other.lectures))
     def __hash__(self): #https://docs.python.org/3.5/reference/datamodel.html#object.__hash__
         return hash((self.courseID,self.title,self.units,self.lectures,self.sections))
+    def setWorkload(self,workload:float):
+        self.workload = workload
+    def updateSectionWorkloads(self,workload):
+        for lecture in self.lectures:
+            lecture.overrideWorkload = float(workload)
+            for section in lecture.sections:
+                section.overrideWorkload = float(workload)
+
     def getSectionLectureCombos(self):
         combos = []
         for lecture in self.lectures:
@@ -190,22 +235,34 @@ class Schedule():
     def getTotalUnits(self) -> float:
         return sum([u for (s,u) in {(section.courseID,section.units) for section in self.sections}])
     def updateInfo(self):
-        self.averageBreak = calculateMedianBreak(self.sections)
         (self.workload,self.instructorScore) = getCourseReview(self.app,self.sections)
     def getWorkload(self) -> float:
+        # if self.workload == None:
+        self.updateInfo()
         return self.workload
     def getInstructorScore(self) -> float:
+        if self.instructorScore == None:
+            self.updateInfo()
         return self.instructorScore
     def getAverageBreak(self) -> float:
+        if self.averageBreak == None:
+            self.averageBreak = calculateMedianBreak(self.sections)
         return self.averageBreak
     def getOverall(self) -> float:
-        pass
+        #10 min median break is most desirable
+        #rating is 0-5 and can make or break a course
+        workloadDiff = max(min(self.getWorkload() - self.getTotalUnits(),-10),10)
+        workloadPercent = (workloadDiff +10)/20
+        ratingPercent = (self.getInstructorScore() - 1)/4
+        breakPercent = min((120 - (self.getAverageBreak()-10)),0)/120 #10min best 120min+ worst
+
+        return pythonRound((workloadPercent*self.app.workloadWeight + ratingPercent*self.app.ratingWeight
+               + breakPercent*self.app.breakWeight)*10,1)
     def __copy__(self): #https://stackoverflow.com/questions/1500718/how-to-override-the-copy-deepcopy-operations-for-a-python-object
         cls = self.__class__
         result = cls.__new__(cls)
         result.__dict__.update(self.__dict__)
         return result
-
     def __deepcopy__(self, memo): #https://stackoverflow.com/questions/1500718/how-to-override-the-copy-deepcopy-operations-for-a-python-object
         cls = self.__class__
         result = cls.__new__(cls)
@@ -289,14 +346,32 @@ class NavBar():
             drawLabel("Required",10,yOff,size=self.app.primaryFontSize,align="left")
             yOff += 10
             for course in self.app.courseGroup["Required"]:
-                yOff += self.drawCourse(course,yOff)
+                newY = self.drawCourse(course,yOff)
+                courseEditButton = Button(lambda x=course: (self.app.state.update({"courseEditPopup":True}),self.app.state.update({"editPopupCourse":x})),self.width-60,7.5+yOff,20,20,fill="White",opacity=50) #self.app.state.update({"editPopupCourseWorkloadInput":str(x.units)})
+                self.app.state["activeButtons"] += [courseEditButton]
+                courseEditButton.draw()
+                drawImage("./assets/pencil-60-16.png",self.width-58,9.5+yOff)
+                courseRemoveButton = Button(lambda x=course: (self.app.courseGroup["Required"].remove(x),generateSchedules(self.app)),self.width-35,7.5+yOff,20,20,fill="White",opacity=50)
+                self.app.state["activeButtons"] += [courseRemoveButton]
+                courseRemoveButton.draw()
+                drawImage("./assets/x-19-16.png",self.width-33,9.5+yOff)
+
+                yOff += newY
         for key in self.app.courseGroup:
             if key == "Required": continue
             yOff += 15
             drawLabel(key,10,yOff,size=self.app.primaryFontSize,align="left")
             yOff += 10
             for course in self.app.courseGroup[key]:
-                yOff += self.drawCourse(course,yOff)
+                newY = self.drawCourse(course,yOff)
+                courseEditButton = Button(lambda x=course: (self.app.state.update({"courseEditPopup":True}),self.app.state.update({"editPopupCourseWorkloadInput":str(x.units)}),self.app.state.update({"editPopupCourse":x})),self.width-60,7.5+yOff,20,20,fill="White",opacity=50)
+                self.app.state["activeButtons"] += [courseEditButton]
+                courseEditButton.draw()
+                drawImage("./assets/pencil-60-16.png",self.width-58,9.5+yOff)
+                courseRemoveButton = Button(lambda x=course,key=key: (self.app.courseGroup[key].remove(x),generateSchedules(self.app)),self.width-35,7.5+yOff,20,20,fill="White",opacity=50)
+                self.app.state["activeButtons"] += [courseRemoveButton]
+                courseRemoveButton.draw()
+                drawImage("./assets/x-19-16.png",self.width-33,9.5+yOff)
                 
     def drawCourse(self, course: Course, yOff: int) -> int: #returns added yOff
         drawRect(10,yOff,self.width-20,35,fill=course.color)
@@ -364,7 +439,7 @@ class CourseView():
             minsEnd = viewPeriod.timeRange[1] + 60  #add 60 because an hour extra is showed on schedule
             minsLength = minsEnd - minsBegin
             # drawRect(xOff,self.yOffset,viewWidth//5,viewHeight-2)
-            self.app.schedules[self.app.state["selectedScheduleIndex"]].updateInfo()
+            
             for section in self.app.schedules[self.app.state["selectedScheduleIndex"]].sections:
                 for day in section.days:
                     beginP = (day.period.timeRange[0] - minsBegin)/minsLength 
@@ -385,7 +460,6 @@ class ScheduleView():
         self.rowHeight = 40
     
     def draw(self):
-        viewWidth = self.app.width - self.xOffset
         # drawLabel("Schedules",self.xOffset + viewWidth//2 - 5,20,size=20)
         drawLabel("ID",self.xOffset +25,30,size=18,bold=True)
         drawLabel("Courses",self.xOffset +200,30,size=18,bold=True)
@@ -395,6 +469,7 @@ class ScheduleView():
         drawLabel("Rating",self.xOffset +580,35,size=18,bold=True )
         drawLabel("Median",self.xOffset +680,15,size=18,bold=True )
         drawLabel("Break",self.xOffset +680,35,size=18,bold=True )
+        drawLabel("Overall",self.xOffset +780,30,size=18,bold=True )
         self.drawSchedules()
     
     def drawSchedules(self):
@@ -406,8 +481,7 @@ class ScheduleView():
             schedule = self.app.schedules[i:i+1]
             if schedule == []:
                 break
-            schedule = schedule[0]#.updateInfo()
-            schedule.updateInfo()
+            schedule = schedule[0]
             drawLine(self.xOffset,yOff,self.xOffset+viewWidth,yOff,lineWidth=.5,fill=rgb(175,175,175))
             drawLabel(i,self.xOffset + 25,yOff + self.rowHeight//2,size=14)
             drawLabel(", ".join(sorted(schedule.getCourseIDs())),self.xOffset + 200,yOff + self.rowHeight//2,size=14,bold=True)
@@ -415,6 +489,7 @@ class ScheduleView():
             drawLabel(schedule.getWorkload(),self.xOffset +480,yOff + self.rowHeight//2,size=16,bold=True)
             drawLabel(schedule.getInstructorScore(),self.xOffset + 580,yOff + self.rowHeight//2,size=16,bold=True)
             drawLabel(schedule.getAverageBreak(),self.xOffset + 680,yOff + self.rowHeight//2,size=16,bold=True)
+            drawLabel(schedule.getOverall(),self.xOffset + 780,yOff + self.rowHeight//2,size=16,bold=True)
             yOff += self.rowHeight
         if len(self.app.schedules) > 0: #border rect
             drawLine(self.xOffset,yOff,self.xOffset+viewWidth,yOff,lineWidth=.5,fill=rgb(175,175,175))
@@ -427,6 +502,34 @@ def redrawAll(app: any):
             app.courseview.draw()
         case "schedules":
             app.scheduleview.draw()
+    drawPopups(app)
+
+def drawPopups(app:any):
+    if app.state["unitPopup"]:
+        app.state["activeButtons"] = [app.popupUnitButton]
+        drawRect(0,0,app.width,app.height,opacity=80)
+        drawRect(app.width//2 - 125, app.height//2 - 100, 250,200, fill="White", border="Black")
+        app.popupUnitButton.draw()
+    elif app.state["courseEditPopup"]:
+        app.state["activeButtons"] = [app.popupCourseSaveButton,app.popupCourseCancelButton]
+        drawRect(0,0,app.width,app.height,opacity=80)
+        drawRect(app.width//2 - 125, app.height//2 - 100, 250,200, fill="White", border="Black")
+        app.popupCourseSaveButton.draw()
+        app.popupCourseCancelButton.draw()
+        drawLabel("Save & Close",app.width//2 +10,app.height//2 + 78,fill="White",size=app.primaryFontSize)
+        drawLabel("Cancel",app.width//2 +90,app.height//2 + 78,fill="White",size=app.primaryFontSize)
+
+        drawLabel("Edit Course: " + app.state["editPopupCourse"].courseID,app.width//2,app.height//2-78,size=app.primaryFontSize)
+
+        #unit input
+        # courseInput = app.state["editPopupCourseWorkloadInput"]
+        # courseFillColor = rgb(48,48,48) if courseInput == "" else "Black"
+        # courseInput = "0" if courseInput == "" else courseInput
+        unitInput = "0" if app.state["editPopupCourseWorkloadInput"] == "" else app.state["editPopupCourseWorkloadInput"]
+        drawRect(5+app.width//2,app.height//2,40, 24,fill="White",border="Black")
+        drawLabel(unitInput,34+app.width//2, app.height//2+12,fill="Black",size=app.primaryFontSize, align="right")
+        drawLabel("Workload:",app.width//2-5, app.height//2+12,fill="Black",size=app.primaryFontSize, align="right")
+
 
 def onMousePress(app, mouseX: int, mouseY:int):
     for button in app.state["activeButtons"]:
@@ -434,7 +537,14 @@ def onMousePress(app, mouseX: int, mouseY:int):
 
 def onKeyPress(app,key):
     app.state["courseInputError"] = ""
-    if key.isnumeric() and app.state["selectedMenu"] =="schedules" and len(app.state["courseInput"]) < 5:
+    if app.state["courseEditPopup"]:
+        if ((key.isnumeric() or (key == "." and "." not in app.state["editPopupCourseWorkloadInput"])) and
+            float(app.state["editPopupCourseWorkloadInput"] + key) <=30 and len( app.state["editPopupCourseWorkloadInput"]) <= 4):
+            app.state["editPopupCourseWorkloadInput"] += key
+        elif key == "backspace":
+            app.state["editPopupCourseWorkloadInput"] = app.state["editPopupCourseWorkloadInput"][:-1]
+
+    elif key.isnumeric() and app.state["selectedMenu"] =="schedules" and len(app.state["courseInput"]) < 5:
         app.state["courseInput"] += key
     elif key.isalpha() and len(key) == 1 and app.state["selectedMenu"] =="schedules" and len(app.state["groupInput"]) < 20:
         app.state["groupInput"] += key
@@ -449,7 +559,7 @@ def onKeyPress(app,key):
         app.state["selectedScheduleIndex"] += 1
     elif key == "left" and app.state["schedulePage"] > 0 and app.state["selectedMenu"] =="schedules":
         app.state["schedulePage"] -= 1
-    elif key == "right" and app.state["schedulePage"] < ((app.height - 50)//40 -1) and app.state["selectedMenu"] == "schedules":
+    elif key == "right" and app.state["schedulePage"] < (len(app.schedules)//((app.height - 50)/40)) and app.state["selectedMenu"] == "schedules":
         app.state["schedulePage"] += 1
 
 def clearCourses(app):
@@ -479,14 +589,21 @@ def addCourse(app:any, groupID: str, courseID: int, init: Optional[bool]=False):
     courseInfo = dict()
     courseInfo["courseID"] = courseID
     courseInfo["title"] = course["Title"]
-    courseInfo["units"] = float(course["Units"])
+    courseInfo["units"] = course["Units"]
     courseInfo["lectures"] = []
     courseInfo["color"] = rgb(*getRandomColor())
 
     courseIndex += 1 #nextLine
     if pd.isnull(courseInfo["units"]):
         course = app.course_df.iloc[courseIndex]
-        courseInfo["units"] = float(course["Units"])
+        courseInfo["units"] = course["Units"]
+
+    if courseInfo["units"] == "VAR":
+        return 
+        # courseInfo["units"] = getCourseUnits(app,courseID)
+        # print("got")
+    else:
+        courseInfo["units"] = float(courseInfo["units"])
 
     allLectures = []
     allSections = []
@@ -519,6 +636,13 @@ def addCourse(app:any, groupID: str, courseID: int, init: Optional[bool]=False):
     if sum([len(app.courseGroup[groupID]) for groupID in app.courseGroup]) >= 4 and not init:
         generateSchedules(app)
 
+def getCourseUnits(app,courseID:str):
+    app.state["unitPopup"] = True
+    app.state["unitPopupCourseID"] = courseID
+    while app.state["unitPopup"]:
+        pass
+
+
 def generateSchedules(app: any):
     courseGroupList = [app.courseGroup[id] for id in app.courseGroup if id != "Required"]
     requiredCourses = app.courseGroup["Required"] if "Required" in app.courseGroup else []
@@ -527,9 +651,7 @@ def generateSchedules(app: any):
     for courseCombos in createCombos(courseGroupList):
         allSchedules += generateSchedulesHelper(requiredCourses + courseCombos,Schedule(app))
     allSchedules = [schedule for schedule in allSchedules if schedule != None]
-    # print(allSchedules)
-    # for schedule in allSchedules:
-    #     schedule.updateInfo()
+
     app.state["selectedScheduleIndex"] = 0
     app.schedules = allSchedules
 
@@ -576,28 +698,35 @@ def calculateMedianBreak(sections: list[Section]):
     return (allBreaks[mindex]+allBreaks[-mindex-1])/2
 
 def getCourseReview(app: any, sections: set[Section]) ->  set[float,float]: 
-    workloads = []
-    ratings = []
-    totalWorkload = None
-    totalRating = None
+    workloads = dict()
+    ratings = dict()
+    totalWorkload = 0
+    totalRating = 4 #default average rating
     for section in sections:
         sectionWorkload = []
         sectionRating = []
-        for instructors in section.getInstructors():
-            if isinstance(instructors,str):
-                for instructor in instructors.split(','):
-                    rating = getProfessorCourseRating(app,section.courseID,instructor.lower())
-                    if rating != None:
-                        sectionWorkload += [rating[0]]
-                        sectionRating += [rating[1]]
+        if section.overrideWorkload != None:
+            sectionWorkload = [section.overrideWorkload]
+        else:
+            for instructors in section.getInstructors():
+                if isinstance(instructors,str):
+                    for instructor in instructors.split(','):
+                        rating = getProfessorCourseRating(app,section.courseID,instructor.lower())
+                        if rating != None:
+                            sectionWorkload += [rating[0]]
+                            sectionRating += [rating[1]]
+        existingLoad = workloads.get(section.courseID,[])
         if len(sectionWorkload) > 0:
-            workloads += [sum(sectionWorkload)/len(sectionWorkload)]
+            workloads.update({section.courseID:[sum(sectionWorkload)/len(sectionWorkload)]+existingLoad})
+        else:
+            workloads.update({section.courseID:[section.units]+existingLoad})
+        existingRating = ratings.get(section.courseID,[])
         if len(sectionRating) > 0:
-            ratings += [sum(sectionRating)/len(sectionRating)]
+            ratings.update({section.courseID:[sum(sectionRating)/len(sectionRating)]+existingRating})
     if len(workloads) > 0:
-        totalWorkload = pythonRound(sum(workloads),2)
+        totalWorkload = pythonRound(sum([sum(v)/len(v) for v in workloads.values()]),2)
     if len(ratings) > 0:
-        totalRating = pythonRound(sum(ratings)/len(ratings),2)
+        totalRating = pythonRound(sum([sum(v)/len(v) if len(v) > 0 else 4 for v in ratings.values()])/len(ratings),2)
     return(totalWorkload,totalRating)
 
 def getProfessorCourseRating(app,courseID,professor):
